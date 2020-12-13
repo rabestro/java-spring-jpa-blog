707572c786e92b08a7b27dc5d3fb8b5b9f4e1b13
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-13 13:27:32 +0200

    Add a List of Posts to the Category Class

--- src/main/java/com/pluralsight/blog/model/Category.java
+++ src/main/java/com/pluralsight/blog/model/Category.java
@@ -10,9 +10,12 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany
+    List<Post> posts;
 
     public Category() {
         super();
+        posts = new ArrayList<>();
     }
 
     public Long getId() {
@@ -28,10 +31,10 @@ public class Category {
     }
 
     public List<Post> getPosts() {
-        return null;
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+        posts.add(post);
     }
 }
